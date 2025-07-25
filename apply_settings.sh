743dbd9e46b80c78f5ab1cec6fcad08c9f3866cc
Wojciech Nagrodzki <278594+wnagrodzki@users.noreply.github.com>
2025-07-25 16:27:44 +0200

    Add "Open Repository in iTerm" palette command

--- apply_settings.sh
+++ apply_settings.sh
@@ -4,3 +4,4 @@ cp Default.sublime-commands ~/Library/Application\ Support/Sublime\ Merge/Packag
 cp "Diff Context.sublime-menu" ~/Library/Application\ Support/Sublime\ Merge/Packages/User/
 
 git config --global alias.openterminalatpath '!f() { open -a Terminal "$1" ; }; f'
+git config --global alias.openitermatpath '!f() { open -a iTerm "$1" ; }; f'
